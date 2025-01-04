2b7a53d549f95e861ebca629859a36072f43978a
George King <george@theorypractice.dev>
2025-01-04 13:52:37 -0800

    caddy/install.sh.

---
+++ server/caddy/install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+function fail() { echo "$1" >&2; exit 1; }
+
+src_dir=$(dirname $0)
+cd "$src_dir"
+
+machine_arch=$(uname -m)
+
+case "${machine_arch}" in
+  "x86_64"|"amd64")
+    caddy_arch="amd64" ;;
+  "aarch64"|"arm64")
+    caddy_arch="arm64" ;;
+  *)
+    fail "Unsupported architecture: ${machine_arch}" ;;
+esac
+
+set -x
+
+caddy_v_version=$(curl -s https://api.github.com/repos/caddyserver/caddy/releases/latest | jq -r '.tag_name')
+caddy_version=${caddy_v_version#v} # Remove "v" prefix.
+
+caddy_dl_dir="caddy_${caddy_version}_linux_${caddy_arch}"
+caddy_dl_name="${caddy_dl_dir}.tar.gz"
+caddy_dl_url="https://github.com/caddyserver/caddy/releases/download/${caddy_v_version}/${caddy_dl_name}"
+
+if ! test -f "${caddy_dl_name}"; then
+  curl -L -o "${caddy_dl_name}" "${caddy_dl_url}"
+fi
+
+tar -xzf "${caddy_dl_name}"
+sudo install "./caddy" /usr/local/bin
