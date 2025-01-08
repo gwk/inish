8882120ea7bc72b38ab2c796befea0bfe75e39dc
George King <george@theorypractice.dev>
2025-01-08 06:27:21 -0800

    caddy/add-security-attrs.sh: set network connection bool on; add var_run_t types to caddy admin socket and autosave files.

--- server/caddy/add-security-attrs.sh
+++ server/caddy/add-security-attrs.sh
@@ -10,10 +10,19 @@ service_user_path='/service/caddy'
 # Allow caddy to serve low-numbered ports.
 sudo setcap cap_net_bind_service=+ep "$bin_path"
 
+sudo setsebool -P httpd_can_network_connect on
+
+
 # Set SELinux context for caddy binary and service home directory.
 sudo semanage fcontext --add --type httpd_exec_t "$bin_path"
 sudo semanage fcontext --add --type httpd_config_t "$service_user_path/Caddyfile"
 sudo semanage fcontext --add --type httpd_sys_content_t "$service_user_path/static/(/.*)?"
+
+sudo semanage fcontext --add --type httpd_var_run_t "$service_user_path/run(/.\*)?"
+#^ The admin socket and autosave.json are placed in a subdirectory with the correct context,
+#^ because caddy creates them and therefore they inherits the context of the parent directory.
+
+
 sudo restorecon -r "$bin_path" "$service_user_path"
 
 # Allow caddy to serve QUIC.
