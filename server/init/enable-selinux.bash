f568b2410984003bb1e4ce078b708e25e00e12e3
George King <george@theorypractice.dev>
2025-01-26 12:21:57 -0800

    enable-selinux.bash.

---
+++ server/init/enable-selinux.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+sudo yum install -y policycoreutils-python-utils python3-policycoreutils
+
+sudo sed -i 's/^SELINUX=permissive/SELINUX=enforcing/' /etc/selinux/config
