b8c80b8b1c1410cc6b43730004192ab5eb12bca0
George King <george@theorypractice.dev>
2025-01-26 13:06:35 -0800

    enable-selinux.bash: setenforce, getenforce.

--- server/init/enable-selinux.bash
+++ server/init/enable-selinux.bash
@@ -6,3 +6,6 @@ set -x
 sudo yum install -y policycoreutils-python-utils python3-policycoreutils
 
 sudo sed -i 's/^SELINUX=permissive/SELINUX=enforcing/' /etc/selinux/config
+
+sudo setenforce 1
+sudo getenforce
