6aefd56f63d721d2f20bf34077d33b2a09f54679
George King <george@theorypractice.dev>
2025-04-09 13:31:48 -0700

    inish python package.

---
+++ inish/__main__.py
@@ -0,0 +1,13 @@
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
+from pithy.argparse import CommandParser
+
+
+def main() -> None:
+  parser = CommandParser(description='Server initialization.')
+
+
+  args = parser.parse_args()
+
+
+if __name__ == '__main__': main()
