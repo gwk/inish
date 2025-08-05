13f28a30f8d56c93a300c75e7849fdbf680a366c
George King <george@theorypractice.dev>
2025-08-05 10:51:00 -0700

    build-litestream.sh: use explicit commit hash.

--- server/src/build-litestream.sh
+++ server/src/build-litestream.sh
@@ -6,13 +6,16 @@ set -x
 src_dir=$(dirname $0)
 cd "$src_dir"
 
-if ! test -f litestream-main.zip; then
-  curl -L -o litestream-main.zip https://github.com/benbjohnson/litestream/archive/refs/heads/main.zip
+litestream_commit="af076fc9f08ee5c81f9fc8d03c2df76dd2336eb4"
+litestream_dir=litestream-$litestream_commit
+litestream_zip=$litestream_dir.zip
+if ! test -f $litestream_zip; then
+  curl -L -o $litestream_zip "https://github.com/benbjohnson/litestream/archive/$litestream_commit.zip"
 fi
 
-rm -rf litestream-main/
-unzip litestream-main.zip
-cd litestream-main
+rm -rf $litestream_dir
+unzip -q $litestream_zip
+cd $litestream_dir
 
 go build ./cmd/litestream
 sudo install litestream /usr/local/bin
